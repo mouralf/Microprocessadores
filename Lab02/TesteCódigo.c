8043f336d3aa9276153c7ff5d277de504d9b1aa1
Luiz <lfm@ufrgs.br>
2021-08-13 19:02:38 -0300

    Separando as coisas

--- Lab02/TesteCódigo.c
+++ Lab02/TesteCódigo.c
@@ -4,56 +4,63 @@ int main()
 {
 
     char LISTA[] = {'N','Y','A','H',' ','H','U','S','T','O','N',0, 7, 90, 9, 11, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0};
-    int notasCompetidores[8] = {0};
+    int notaIndividual[8] = {0};
     float notasParticipantes[8];
     int i = 0, k = 0;
     char nomes[8][15];
 
-    while(LISTA[i] != 0){
-            nomes[k][i] = LISTA[i];
-            i++;
+    //percorre a LISTA[]
+    for(int numCompetidores = 0; numCompetidores<8; numCompetidores++){
+            //pega o nome do skatista
+            while(LISTA[i] != 0){
+                    nomes[k][i] = LISTA[i];
+                    i++;
+            }
+            //apenas exibe o nome do cara. Apagar depois
+            int o = 0;
+            while(LISTA[o] != 0){
+                printf("%c", nomes[k][o]);
+                o++;
+            } //apagar até aqui
+
+
+
     }
 
-    int o = 0;
-     while(LISTA[o] != 0){
-        printf("%c", nomes[k][o]);
-        o++;
-    }
+
 
     //pega as notas do competidor
-    for (int l = 0; l<14 ; l++)
+    for (int todasNotas = 0; todasNotas<14 ; todasNotas++)
     {
-        if(l%2 != 0) //ta na unidade decimal
+        if(todasNotas%2 != 0) //ta na unidade decimal
         {
-            notasCompetidores[(l-1)/2] = (LISTA[l-1])*100+LISTA[l];
+            notaIndividual[(todasNotas-1)/2] = (LISTA[todasNotas-1])*100+LISTA[todasNotas];
         }
     }
 
-    printf("\nAgora vamo ver se deu certo essa merda!\n");
-
-    //exibe as 7 notas do infeliz
+    //exibe as 7 notas do infeliz. Apagar depois
     for (int q = 0; q<7; q++)
     {
-        printf("Nota: %d\t", notasCompetidores[q]);
-    }
+        printf("Nota: %d\t", notaIndividual[q]);
+    } //apagar até aqui
 
     //ordena em ordem decrescente
     for (int m = 1; m < 8; m++)
     {
         for (int n = 0; n < m; n++)
         {
-            if (notasCompetidores[m] > notasCompetidores[n])
+            if (notaIndividual[m] > notaIndividual[n])
             {
-                int temp = notasCompetidores[m];
-                notasCompetidores[m] = notasCompetidores[n];
-                notasCompetidores[n] = temp;
+                int temp = notaIndividual[m];
+                notaIndividual[m] = notaIndividual[n];
+                notaIndividual[n] = temp;
             }
         }
     }
 
     for (int p = 0; p<7; p++)
     {
-        printf("\nNota ordenada[%d]: %d\n", p, notasCompetidores[p]);
+        printf("\nNota ordenada[%d]: %d\n", p, notaIndividual[p]);
     }
 
     for (int indiceSkatista = 0; indiceSkatista<8; indiceSkatista++ )
@@ -64,8 +71,8 @@ int main()
         //soma as 4 melhores notas
         for (int indiceNota = 0; indiceNota<4; indiceNota++)
         {
-            printf("Nota[%d]: %d \t", indiceNota, notasCompetidores[indiceNota]);
-            notaFinal+=notasCompetidores[indiceNota];
+            printf("Nota[%d]: %d \t", indiceNota, notaIndividual[indiceNota]);
+            notaFinal+=notaIndividual[indiceNota];
             //printf("\nNota final parcial: %f\t", notaFinal);
 
         }
