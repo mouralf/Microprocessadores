cff5a47bc4ad4507e63a91cf106dd2936d2c9ee3
Luiz <lfm@ufrgs.br>
2021-08-13 15:55:16 -0300

    Pegando as notas e listando quase ordenado

---
+++ Lab02/Chorão.c
@@ -0,0 +1,52 @@
+char code LISTA[] = {
+'N','Y','A','H',' ','H','U','S','T','O','N',0, 7, 90, 9, 11, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0,  //7,90; 9,11; 0,0; 0,0; 0, 0; 0,0
+'J','A','G','G','E','R',' ','E','A','T','O','N',0, 8, 20, 9, 5, 0, 0, 8, 70,9,40, 0, 0, 0, 0, //8,20; 9,05; 0,0; 8,70; 9,40; 0,0; 0,0
+'G','U','S','T','A','V','O',' ','R','I','B','E','I','R','O',0, 7, 23, 5, 82, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0,  //7,23; 5,82; 0,0; 0,0; 0,0; 0,0; 2,0
+'K','E','L','V','I','N',' ','H','O','E','F','L','E','R',0, 8, 98, 8, 84, 8, 99, 0, 0, 0, 0, 7,58, 9,34, //8,98; 8,84; 8,99; 0,0; 0,0; 7,58; 9,34
+'V','I','C','E','N','T',' ','M','I','L','O','U',0, 7, 87, 5, 54, 9, 23, 0, 0, 8, 34, 0,0, 8,70, //7,87; 5,54; 9,23; 0,0; 8,34; 0,0; 8,70
+'A','N','G','E','L','O',' ','N','A','R','V','A','E','Z',0, 7, 1, 6, 9, 9, 0, 0, 0, 0, 0, 8,65, 8,21, //7,01; 6,89; 9,00; 0,0; 0,0; 8,65; 8,21
+'A','U','R','E','L','I','A','N',' ','G','R','A','U','D',0, 4, 21, 7, 20, 8, 68, 0, 0, 9, 0, 0, 0, 0, 0, //4,21; 7,20; 8,68; 0,0; 9,0; 0,0;
+'Y','U','T','O',' ','H','O','R','I','G','A','M','E',0, 8, 02, 6, 77, 9, 3, 0, 0, 9, 35, 9, 50, 9,30 //8,02; 6,77; 9,03; 0,0; 9,35; 9,50; 9,30
+};
+
+
+char RESULT[100] ;
+
+//AS QUATRO MELHORES NOTAS SÃO SOMADAS PARA DETERMINAR A PONTUAÇÃO
+
+void main()
+{
+// seu código
+while(1) ; // Final do programa 
+}
+
+//lista pro nome
+
+//lista pras notas
+char 
+
+//laço para percorrer as notas
+for (int i = 0; i<13 ; i++){
+	if (i%2 != 0){ //é índice ímpar do arrya, o que significa que é o dígito decimal da nota, então pode-se formar a nota
+		notaAtual = LISTA[I]
+}
+
+
+
+
+
+//verifica as maiores notas
+
+//o nome é delimitado pelo caracter 0
+
+//a cada uma vírgula forma-se a nota
+
+char 	nota1[] = 0, 
+			nota2[] = 0, 
+			nota3[] = 0, 
+			nota4[] = 0;
+
+float notaFinal = 0, //armazena a nota do competidor e compara com as notas do pódio
+			notaOuro = 0, 
+			notaPrata = 0, 
+			notaOuro = 0;
---
+++ Lab02/TesteCódigo.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char LISTA[] = {7, 90, 9, 11, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0};
+    int notasCentesimal[7], notasOrdenadas[7];
+    int nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0;
+    int notasParticipantes[8];
+
+    //pega as notas do competidor
+    for (int i = 0; i<14 ; i++)
+    {
+        if(i%2 != 0){//ta na unidade decimal
+                notasCentesimal[(i-1)/2] = (LISTA[i-1])*100+LISTA[i];
+        }
+
+    }
+
+    printf("\nAgora vamo ver se deu certo essa merda!\n");
+
+    //exibe as 7 notas do infeliz
+    for (int j = 0; j<7; j++){
+        printf("Nota: %d\n", notasCentesimal[j]);
+    }
+
+     for (int i = 1; i < 8; i++) {
+        for (int j = 0; j < i; j++) {
+            if (notasCentesimal[i] > notasCentesimal[j]) {
+                int temp = notasCentesimal[i];
+                notasCentesimal[i] = notasCentesimal[j];
+                notasCentesimal[j] = temp;
+            }
+        }
+    }
+
+      for (int j = 0; j<7; j++){
+        printf("\nNota ordenada[%d]: %d\n", j, notasCentesimal[j]);
+    }
+
+
+    return 0;
+}
+
+
