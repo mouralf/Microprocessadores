a774c400a61b9b8fa2881dd1a4c9eb2b6fb308b2
Luiz <lfm@ufrgs.br>
2021-08-13 17:40:44 -0300

    Mudei as variáveis :)

--- Lab02/TesteCódigo.c
+++ Lab02/TesteCódigo.c
@@ -4,39 +4,59 @@ int main()
 {
 
     char LISTA[] = {7, 90, 9, 11, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0};
-    int notasCentesimal[8] = {0};
-    int notasOrdenadas[7];
-    int nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0;
-    int notasParticipantes[8];
+    int notasCompetidores[8] = {0};
+    float notasParticipantes[8];
 
     //pega as notas do competidor
     for (int i = 0; i<14 ; i++)
     {
-        if(i%2 != 0){//ta na unidade decimal
-                notasCentesimal[(i-1)/2] = (LISTA[i-1])*100+LISTA[i];
+        if(i%2 != 0) //ta na unidade decimal
+        {
+            notasCompetidores[(i-1)/2] = (LISTA[i-1])*100+LISTA[i];
         }
-
     }
 
     printf("\nAgora vamo ver se deu certo essa merda!\n");
 
     //exibe as 7 notas do infeliz
-    for (int j = 0; j<7; j++){
-        printf("Nota: %d\n", notasCentesimal[j]);
+    for (int j = 0; j<7; j++)
+    {
+        printf("Nota: %d\n", notasCompetidores[j]);
     }
 
-     for (int i = 1; i < 8; i++) {
-        for (int j = 0; j < i; j++) {
-            if (notasCentesimal[i] > notasCentesimal[j]) {
-                int temp = notasCentesimal[i];
-                notasCentesimal[i] = notasCentesimal[j];
-                notasCentesimal[j] = temp;
+    //ordena em ordem decrescente
+    for (int i = 1; i < 8; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (notasCompetidores[i] > notasCompetidores[j])
+            {
+                int temp = notasCompetidores[i];
+                notasCompetidores[i] = notasCompetidores[j];
+                notasCompetidores[j] = temp;
             }
         }
     }
 
-      for (int j = 0; j<7; j++){
-        printf("\nNota ordenada[%d]: %d\n", j, notasCentesimal[j]);
+    for (int j = 0; j<7; j++)
+    {
+        printf("\nNota ordenada[%d]: %d\n", j, notasCompetidores[j]);
+    }
+
+    for (int indiceSkatista = 0; indiceSkatista<8; indiceSkatista++ )
+    {
+        //colocar isso aqui dentro dum for pra armazenar pra cada participante
+        int notaFinal = 0;
+        //soma as 4 melhores notas
+        for (int indiceNota = 0; indiceNota<4; indiceNota++)
+        {
+            printf("\nNota[%d]: %d", indiceNota, notasCompetidores[indiceNota]);
+            notaFinal+=notasCompetidores[indiceNota];
+
+        }
+        //atribui a nota de cada participante ao array
+        notasParticipantes[indiceSkatista] = (notaFinal)/100;
+        printf("\nNota final [%d]: %f", indiceSkatista, notasParticipantes[indiceSkatista]);
     }
 
 
