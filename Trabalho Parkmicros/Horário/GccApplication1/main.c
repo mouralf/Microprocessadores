fc7d9a547ce886012fa23b4e3715127e9f3a46bb
Luiz <lfm@ufrgs.br>
2021-10-01 09:24:50 -0300

    Código do Will

---
+++ Trabalho Parkmicros/Horário/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 01/10/2021 09:19:47
+ * Author : luiz-
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
