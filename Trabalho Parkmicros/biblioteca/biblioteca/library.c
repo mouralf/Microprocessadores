a9b1a24e0f78e016d1a937200db3850c60e930c5
PedroBaitelli <68336633+PedroBaitelli@users.noreply.github.com>
2021-10-22 05:55:28 -0300

    POGNOCHATFAMILIA

---
+++ Trabalho Parkmicros/biblioteca/biblioteca/library.c
@@ -0,0 +1,16 @@
+/*
+ * biblioteca.c
+ *
+ * Created: 22/10/2021 00:05:14
+ * Author : pbatm
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
