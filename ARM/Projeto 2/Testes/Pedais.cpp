9e16d37f2222fc18a5996087cb9535eea56408b2
Luiz <lfm@ufrgs.br>
2021-11-16 16:44:15 -0300

    Ajustes nos Pedais

--- ARM/Projeto 2/Testes/Pedais.cpp
+++ ARM/Projeto 2/Testes/Pedais.cpp
@@ -6,35 +6,34 @@ DigitalIn btnFreio(p12);    //define o botão conectado ao pino 13 (Freio) como 
 
 PwmOut ledInjecao(p23); //define o pino 23 como saída PWM
 
-float dcInjecao = 0;    //variável pra fazer o controle do duty cycle da injeção
-
-Ticker timerPedais; //interrupção que sreá chamada repetidamente pra ler os pedais
+Ticker timerPedais; //interrupção que será chamada repetidamente pra ler os pedais
 
 //função que será chamada a cada 0.5 s pra verificar o estado dos pedais
 void LeituraPedais(){
     //verifica se o freio foi pressionado
     if (btnFreio.read()){
-        dcInjecao = dcInjecao - 0.5; //decrementa o duty cycle em 50%
+        ledInjecao = ledInjecao - 0.5; //decrementa o duty cycle em 50%
     }
     //se não estiver pressionado, verifica se o acelerador foi pressionado
     else if (btnAcelerador.read()){
-        dcInjecao = dcInjecao + 0.1; //incrementa o duty cycle
+        ledInjecao = ledInjecao + 0.1; //incrementa o duty cycle
     }
     //se nenhum tiver sido pressionado
     else{
-        dcInjecao = dcInjecao - 0.1;
-        wait(1); //espera 1s
+        ledInjecao = ledInjecao - 0.1;
+        //espera 1s
+        wait(0.5f); //como a funçao é chamada a cada 0.5 s, então se não tiver nada pressionado espera 0.5 s
     }
-    printf("\nDuty cycle injecao: %f \n", dcInjecao);
+    printf("\nDuty cycle injecao: %f \n", ledInjecao);
 
     //impede que o duty cycle fique negativo
-    if(dcInjecao <= 0){
-        dcInjecao = 0;
+    if(ledInjecao <= 0){
+        ledInjecao = 0;
     }
 
     //impede que o duty cycle fique acima de 100%
-    if(dcInjecao >= 1){
-        dcInjecao = 1;
+    if(ledInjecao >= 1){
+        ledInjecao = 1;
     }
     
 }
