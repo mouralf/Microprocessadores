da984864b3a3db75e4caddb57aecc40b1cf90aff
Luiz <lfm@ufrgs.br>
2021-11-17 13:16:09 -0300

    Funções dos cornos

--- ARM/Projeto 2/Testes/DisplayLCD.cpp
+++ ARM/Projeto 2/Testes/DisplayLCD.cpp
@@ -11,21 +11,21 @@ DigitalIn btnConfig(p11);
 //função utilizada para indicar o que deve ser exibido no display (velocidade, quilometragem, umidade)
 
 char exibicao = 0; //será incrementado para realizar o controle do que será exibido
+
 void SelecionaFuncao(){
     exibicao++;
 
-    if(exibicao == 4){
+    /*
+    exibicao = 0 -> quilometragem
+    exibicao = 1 -> velocidade instantanea
+    exibicao = 2 -> velocidade media
+    exibicao = 3 -> temperatura 
+    exibicao = 4 -> umidade
+    */
+
+    if(exibicao == 5){
         exibicao = 0;  //zera exibicao pra não passar das opções disponíveis
     }
-    
-    /*
-    exibicao = 0 -> velocidade instantanea
-    exibicao = 1 -> velocidade media
-    exibicao = 2 -> quilometragem
-    exibicao = 3 -> temperatura e umidade
-    */
-    ExibeMssg();    //chama função que escreve no display
-
    
 }
 
@@ -35,42 +35,6 @@ void ConfiguraValores(){
 
 }
 
-
-void ExibeMssg(){
-    lcd.cls();  //sempre que for mudar a mssg de exibição limpa a tela
-
-    switch (exibicao)
-    {
-        case 0: //velocidade instantanea
-            lcd.printf("Velocidade inst.");
-            lcd.locate(1, 12); //coloca o cursor na segunda linha
-            lcd.printf("%d ", velocInst);  //printa a velocidade instantanea
-            lcd.printf(" km/h");
-            break;
-
-        case 1: //velocidade media
-            lcd.printf("Velocidade media");
-            lcd.locate(1, 2); //coloca o cursor na segunda linha
-            lcd.printf("%d ", velocMedia);  //printa a velocidade instantanea
-            lcd.printf("km/h");
-            break;
-        
-        case 2: //quilometragem
-            lcd.printf("Quilometragem");
-            lcd.locate(1, 12); //coloca o cursor na segunda linha
-            lcd.printf("%d ", quilometragem);  //printa a velocidade instantanea
-            lcd.printf("m");
-
-            break;
-
-        case 3: //temperatura e umidade
-            lcd.printf("Temp.: %d oC", temperatura);    //printa a temperatura
-            lcd.locate(1, 12); //coloca o cursor na segunda linha
-            lcd.printf("Umid.: %d %", umidade);  //printa a umidade
-            break;
-    }
-
-}
 
 /*============================== Função principal ==============*/
 
